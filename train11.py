2ac45bf541fb6e7453bdbd3957db8a7050793a59
Rongji Nie <46857034+NRJ2019120@users.noreply.github.com>
2019-03-05 15:38:52 +0800

    Update train11.py
    关键点坐标训练样本将之前的正样本修改为正样本和部分样本

--- train11.py
+++ train11.py
@@ -3,7 +3,6 @@ import os
 import torch.nn as nn
 import sampling as samp
 import torch.utils.data as data
-import numpy as np
 
 class Trainer(nn.Module):   #nn.Module 所有模型的父类
 
@@ -20,9 +19,9 @@ class Trainer(nn.Module):   #nn.Module 所有模型的父类
             self.net.cuda()
     def train(self):
 
-        dataset = samp.FaceDataset(self.rootpath)
+        dataset = samp.FaceDataset(self.rootpath,islandmark=True)
         self.dataloader = data.DataLoader(dataset, batch_size=512, shuffle=True,num_workers=4)
-        self.optimer = torch.optim.Adam(self.net.parameters(),lr=1e-4,weight_decay=0.00005)#weight_decay  正则化因子
+        self.optimer = torch.optim.Adam(self.net.parameters(),lr=1e-4,weight_decay=0.00005) #weight_decay  正则化因子
 
         if os.path.exists(self.parampath):  # 恢复参数继续训练
             self.net.load_state_dict(torch.load(self.parampath))
@@ -32,11 +31,11 @@ class Trainer(nn.Module):   #nn.Module 所有模型的父类
                 os.makedirs(dirs)
         #训练
         while True:
-           for epoch,(imgdata, con, offset) in enumerate(self.dataloader):  # batchsize
+           for epoch,(imgdata, con, offset,landmarks) in enumerate(self.dataloader):  # batchsize
             if self.isCuda == True:              #GPU 运算
-                imgdata, con, offset = imgdata.cuda(), con.cuda(), offset.cuda()#,landmarks.cuda()
+                imgdata, con, offset,landmarks = imgdata.cuda(), con.cuda(), offset.cuda(),landmarks.cuda()
 
-            con_out, offset_out = self.net(imgdata)   # imgdata为网络输入, 得到网络输出
+            con_out, offset_out,landmarks_out = self.net(imgdata)   # imgdata为网络输入, 得到网络输出
              #如何保证con_out  > 0???        # Assertion `x >= 0. && x <= 1.' failed. input value should be between 0~1, but got -0.157099 at
                                              # 加signmod 函数 转化成概率
             # 计算置信度损失
@@ -55,26 +54,24 @@ class Trainer(nn.Module):   #nn.Module 所有模型的父类
             offset_out_ = offset_out[offset_mask[:,0]]
             offset_loss = self.offset_lossfun(offset_out_, offset_)  # 只有非负样本参与坐标偏移损失计算
 
-            # landmarks_ = landmarks[offset_mask[:,0]]            #con>0 包含正样本和正样本
-            # # print(landmarks_.shape)
-            # landmarks_out_ = landmarks_out[offset_mask[:, 0]]   #con>0 包含正样本和正样本
-            #
-            # landmarks_mask = torch.lt(offset_,2)                   # 只选正样本 con >0  and con <2
-            # landmarks_ = landmarks_[landmarks_mask[:,0]]
-            # landmarks_out_ = landmarks_out_[landmarks_mask[:, 0]]
-            # # print(landmarks_mask)
-            # # landmarks_ = landmarks[offset_mask[:,0]]
-            # # landmarks_out_ = landmarks_out[offset_mask[:,0]]
-            # landmarks_loss = self.landmarks_lossfun(landmarks_out_,landmarks_)
+            #计算关键点损失
+            landmarks_mask = torch.gt(con,0)                   #正样本和部分样本训练关键点
+            landmarks_ = landmarks[landmarks_mask[:,0]]
+            landmarks_out_ = landmarks_out[landmarks_mask[:, 0]]
+            # print(landmarks_mask)
+            landmarks_loss = self.landmarks_lossfun(landmarks_out_,landmarks_)
             #总loss
-            total_loss = con_loss + offset_loss #+ landmarks_loss
+            total_loss = con_loss + 0.5*offset_loss + landmarks_loss
 
             self.optimer.zero_grad()
             total_loss.backward()
             self.optimer.step()
             # break
             if epoch % 100 == 0 and epoch>0:
+                print("epoch==>",epoch,"total_loss==>",total_loss)
+            if epoch % 500 == 0 and epoch>0:
                 torch.save(self.net.state_dict(),self.parampath)  #保存模型参数
+                print("===========save module===============")
                 print("epoch==>",epoch,"total_loss==>",total_loss)
             # if epoch % 10000 == 0:
             #     feed = input("whether continue or over train YES OR NO")
@@ -84,7 +81,8 @@ class Trainer(nn.Module):   #nn.Module 所有模型的父类
             #         break
 
 # if __name__ == '__main__':
-#     net = MTCNNnet.P_NET()
+#
+#     net = torchnet.O_NET()
 #     net.train()  # 参数参与更新,如果是测试使用则是 net.eval().  r"Sets the module in training mode".
 #
 #     onet_trainer = Trainer(net,P_parampath,root_path12,isCuda=False)
